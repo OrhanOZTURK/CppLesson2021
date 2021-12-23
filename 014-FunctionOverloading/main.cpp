efd20ee8a38be6e1def1dd0f94eefae4afbedd9e
OrhanOZTURK <orhn.ozturk@gmail.com>
2021-12-24 00:17:39 +0300

    FunctionOverloading

---
+++ 014-FunctionOverloading/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+//Soru F.O --> function overloading mi vs redeclaration mı
+
+//F.O
+
+int func(int);
+int func(bool);
+
+int main()
+{
+
+    return 0;
+}
+
+
+/*********************************************/
+/*********************************************/
+
+
+
