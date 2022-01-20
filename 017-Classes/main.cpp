49c0d7a606fab794ba5cdf50a9d38a7aff2b69a3
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-01-21 00:56:44 +0300

    Class

--- 017-Classes/main.cpp
+++ 017-Classes/main.cpp
@@ -2,47 +2,45 @@
 
 using namespace std;
 
-
-//.hpp
-
+//hpp
 class Myclass{
-private:
-    int mx[5]{};
-
 public:
-    Myclass(){
-        cout << "Myclass default ctor ... this : " << this << endl;
+    void func()const
+    {
+        ++count;
     }
 
-    ~Myclass(){
-       cout << "Myclass default dtor ... this : " << this << endl;
-    }
+private:
+    mutable int count;
 };
-
-//.cpp
-
-void func(Myclass r){
-
-}
-
 
 int main()
 {
-    cout << "main basliyor\n";
 
-    Myclass m;
-
-    //copy ctor cagrılıyor
-    //ileriki derlerde cevabımızı alacaz
-    for (int i = 0; i < 10; ++i ) {
-        func(m);
-    }
-
-    cout << "main sona eriyor\n";
 
     return 0;
 }
 
 /*********************************************/
 /*********************************************/
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
