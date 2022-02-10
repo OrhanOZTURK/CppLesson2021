7f929ef8301ac96721c919d420b14578f061c8f1
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-02-10 23:38:08 +0300

    Move Ctor - move Assignment

--- 017-Classes/main.cpp
+++ 017-Classes/main.cpp
@@ -6,16 +6,49 @@
 using namespace std;
 
 //hpp
+
 class Myclass{
+private:
+   int mx = 0;
+
 public:
-    Myclass();
-    ~Myclass();
-    Myclass(const Myclass &other);
-    Myclass &operator=(const Myclass &other);
+    Myclass() : mx(0)
+    {
+        cout << "Myclass() this : " << this << "\n";
+    }
+
+    Myclass(int val) : mx{val}
+    {
+        cout << "Myclass(int val) val : "<< val << " this : " << this << "\n";
+    }
+
+    ~Myclass(){
+        cout << "Myclass dtor this : " << this << "\n";
+    }
+
+
 };
+
+
+void func(Myclass &&r) //Rvalue
+{
+    cout << "func Myclass && cagrildi\n";
+}
+
+void func(const Myclass &r) //Lvalue
+{
+    cout << "func const Myclass & cagrildi\n";
+}
 
 int main()
 {
+    cout << "Main basladı\n";
+
+    Myclass m{2};
+
+    func(m);
+
+    cout << "Main devam ediyor\n";
 
     return 0;
 }
