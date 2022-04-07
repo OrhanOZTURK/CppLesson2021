09068ab52565a7db7a6ff45b6f82aebb5b9a7897
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-04-08 01:29:09 +0300

    string and initializer_list

--- 017-Classes/main.cpp
+++ 017-Classes/main.cpp
@@ -1,30 +1,25 @@
 #include <iostream>
+#include <initializer_list>
+#include <vector>
 #include <string>
 
 using namespace std;
 
-void display_string(const string &s)
-{
-    cout << "[" << s.length() << "] (" << s << ")\n";
-}
-
 
 int main()
 {
-    char str[] = {"0123456789"};
+    string str;
 
-    string s1(str+2, 5);
-    string s2{str + 2, str+5};
+    str = {'A', 'B', 'C'};
 
-    display_string(s1);
-    display_string(s2);
-
+    cout << str.size();
 }
 
 
+/***************************************************/
+/***************************************************/
 
-/***************************************************/
-/***************************************************/
+
 
 
 
