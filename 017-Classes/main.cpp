b225cebe4f549fe245e30fc9b4594bcaeec8c05a
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-05-24 21:39:45 +0300

    try catch

--- 017-Classes/main.cpp
+++ 017-Classes/main.cpp
@@ -1,61 +1,23 @@
 #include <iostream>
 #include "utility.hpp"
-#include <cstdlib>
-#include <string>
 #include <stdexcept>
+#include "car.hpp"
+#include <memory>
+#include <vector>
 
 using namespace std;
 
-
-void f4()
+void foo()
 {
-    cout << "f4 basladi\n";
-
-    std::string str{"orhan"};
-
-    str.at(30) = 'X'; //out of range
-
-    cout << "f4 sona erdi\n";
-}
-
-
-void f3()
-{
-    cout << "f3 basladi\n";
-    f4();
-    cout << "f3 sona erdi\n";
-}
-
-
-void f2()
-{
-    cout << "f2 basladi\n";
-    f3();
-    cout << "f2 sona erdi\n";
-}
-
-
-void f1()
-{
-    cout << "f1 basladi\n";
-    f2();
-    cout << "f1 sona erdi\n";
+    cout << "foo cagrildi  ve foo hata nesnesini tekrar gonderiyor\n";
+    throw;
 }
 
 
 int main()
 {
-    set_terminate(myabort);
-
-    cout << "main baslad\n";
-
-    try {
-        f1();
-    }  catch (std::exception &ex) { //kalıtım yapıası var
-        cout << "hata yakalandi  = " << ex.what() << "\n";
-    }
-
-    cout << "main sona eriyor\n";
+    set_terminate(&myabort);
+    foo();
 }
 
 /***************************************************/
