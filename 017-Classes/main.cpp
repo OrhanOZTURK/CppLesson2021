e489cc1adf22c1862ea06b3168dfabb67f14d72f
OrhanOZTURK <orhn.ozturk@gmail.com>
2022-07-19 22:08:17 +0300

    template class and std::pair interface 1

--- 017-Classes/main.cpp
+++ 017-Classes/main.cpp
@@ -3,39 +3,13 @@
 
 using namespace std;
 
-//std::pair basit interface yapısının gösterimi
-
-template<typename T, typename U>
-struct Pair{
-
-    T first;
-    U second;
-
-    Pair() : first(), second(){};
-    Pair(const T &t, const U &u) : first(t), second(u){};
-
-};
-
-template<typename T, typename U>
-bool operator<(const Pair<T,U> &p1, const Pair<T,U> &p2)
-{
-    return p1.first < p2.first || !(p2.first < p1.first && p1.second < p2.second);
-}
-
-template<typename T, typename  U>
-std::ostream & operator<<(std::ostream &os, const Pair<T, U> &p)
-{
-    return os << "(" << p.first << ", " << p.second << ")" << "\n";
-}
-
 
 int main()
 {
-    Pair<int, double> px{12, 4.2};
-    Pair<string, string> py{"mehmet", "nazim"};
+    std::pair<double, long> px;
+    std::pair<float, int> py{12.4, 12};
 
-
-    cout << px << py << "\n";
+    px = py;
 
 
 }
